8c6ed898ef0dc521d7ca7f8c2069337556cac555
Anup <anuppoudel60@gmail.com>
2026-09-22 08:28:46 +1000

    cors added

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,12 @@ import postRouter from "./routes/postRoute.js";
 import userRouter from "./routes/userRoutes.js";
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 const port = process.env.PORT || 5000;
